5d0a6551822893ab8ddf1f86ac72d3772667f44f
Ian Maurmann <ikmxx@yahoo.com>
2024-02-27 22:59:46 -0700

    Add resource pack

---
+++ src/fa-resource-pack/FaIconsResourcePack.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * FA Icons Resource Pack
+ * ----------------------
+ *
+ * @noinspection PhpClassNamingConventionInspection    - Long class names are ok.
+ * @noinspection PhpPropertyNamingConventionInspection - Property names with underscores are ok.
+ * @noinspection PhpUnused                             - Ignore that pack_type is unused.
+ */
+
+
+declare(strict_types=1);
+
+namespace PithFront\PithPackFaIcons;
+
+use Pith\Workflow\PithPack;
+
+/**
+ * Class FaIconsResourcePack
+ */
+class FaIconsResourcePack extends PithPack
+{
+    public string $access_level = 'world';
+    public string $pack_type    = 'resource-pack';
+}
